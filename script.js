863d53e83c86766f088654917ce2973a5b5824df
Liang <1878464932@qq.com>
2023-01-14 19:44:29 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         必应搜索去广告
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Liang
+// @match        *.bing.com/search?q=*
+// @include      *.bing.com/search?q=*
+// @icon         https://cn.bing.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    var a = 1;
+
+    for(var i = 1;i <= 10; i += 1){
+        setTimeout(()=>{
+            removeAd(a++);
+        }, i*1000);
+    }
+
+    function removeAd(a){
+        var ad = document.querySelector(".b_ad");
+        var ad2 = document.querySelectorAll('#b_results li>h2');
+
+        ad.style.display = 'none';
+
+        ad2[0].parentElement.style.display = 'none';
+        ad2[1].parentElement.style.display = 'none';
+    }
+    
+})();
